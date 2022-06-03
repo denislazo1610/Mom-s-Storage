656732a0a1e4d0969578fbb6fef6c73dcc99715a
Denis Lazo <denislazo1610@gmail.com>
2022-06-03 00:03:24 -0600

    heroku with auth0

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET,
-  baseURL: "https://momsstore.herokuapp.com",
+  baseURL: "http://localhost:3000",
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: `https://${process.env.ISSUER_BASE_URL}`,
 };
